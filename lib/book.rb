546c51cc86cd8fb54138436c20bf9cb14e686397
Samara Grant <samara.amanda01@gmail.com>
2020-07-26 01:39:05 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -4,39 +4,11 @@ class Book
     @title = title
   end
 
-  def title
-    @title
-  end
-
-  def author=(author)
-    @author = author
-  end
-
-  def author
-    @author
-  end
-
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-
-  def page_count
-    @page_count
-  end
-
-  def genre=(genre)
-    @genre = genre
-  end
-
-  def genre
-    @genre
-  end
+  attr_reader :title
+  attr_accessor :author, :page_count, :genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-
-
-
 
 end
